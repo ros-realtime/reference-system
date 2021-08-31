301226ee07009013c4d69fff53b8968245aaef8d
Evan Flynn <evan.flynn@apex.ai>
2021-08-31 11:50:00 -0700

    fix lint issues, add static single threaded executor
    Signed-off-by: Evan Flynn <evan.flynn@apex.ai>

--- reference_system_autoware/include/reference_system_autoware/node/processing.hpp
+++ reference_system_autoware/include/reference_system_autoware/node/processing.hpp
@@ -1,3 +1,17 @@
+// Copyright 2021 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include <chrono>
@@ -7,40 +21,45 @@
 #include "reference_system_autoware/number_cruncher.hpp"
 #include "reference_system_autoware/types.hpp"
 
-namespace node {
-struct ProcessingSettings {
+namespace node
+{
+struct ProcessingSettings
+{
   std::string node_name;
   std::string input_topic;
   std::string output_topic;
   std::chrono::nanoseconds number_crunch_time;
 };
 
-class Processing : public rclcpp::Node {
- public:
-  Processing(const ProcessingSettings& settings)
-      : Node(settings.node_name),
-        number_crunch_time_(settings.number_crunch_time) {
+class Processing : public rclcpp::Node
+{
+public:
+  Processing(const ProcessingSettings & settings)
+  : Node(settings.node_name),
+    number_crunch_time_(settings.number_crunch_time)
+  {
     subscription_ = this->create_subscription<message_t>(
-        settings.input_topic, 10,
-        [this](const message_t::SharedPtr msg) { input_callback(msg); });
+      settings.input_topic, 10,
+      [this](const message_t::SharedPtr msg) {input_callback(msg);});
     publisher_ = this->create_publisher<message_t>(settings.output_topic, 10);
   }
 
- private:
-  void input_callback(const message_t::SharedPtr input_message) const {
+private:
+  void input_callback(const message_t::SharedPtr input_message) const
+  {
     auto number_cruncher_result = number_cruncher(number_crunch_time_);
 
     auto output_message = publisher_->borrow_loaned_message();
     const int64_t timestamp_in_ns =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(
-            std::chrono::system_clock::now().time_since_epoch())
-            .count();
+      std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::system_clock::now().time_since_epoch())
+      .count();
 
     const int64_t input_accumulated_latency = input_message->data[0];
     const int64_t input_timestamp = input_message->data[1];
 
     const int64_t accumulated_latency_in_ns =
-        input_accumulated_latency + timestamp_in_ns - input_timestamp;
+      input_accumulated_latency + timestamp_in_ns - input_timestamp;
 
     output_message.get().data[0] = accumulated_latency_in_ns;
     output_message.get().data[1] = timestamp_in_ns;
@@ -49,7 +68,7 @@ class Processing : public rclcpp::Node {
     publisher_->publish(std::move(output_message));
   }
 
- private:
+private:
   publisher_t publisher_;
   subscription_t subscription_;
   std::chrono::nanoseconds number_crunch_time_;
--- reference_system_autoware/include/reference_system_autoware/node/sensor.hpp
+++ reference_system_autoware/include/reference_system_autoware/node/sensor.hpp
@@ -1,3 +1,17 @@
+// Copyright 2021 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include <chrono>
@@ -6,28 +20,35 @@
 #include "rclcpp/rclcpp.hpp"
 #include "reference_system_autoware/types.hpp"
 
-namespace node {
-struct SensorSettings {
+namespace node
+{
+struct SensorSettings
+{
   std::string node_name;
   std::string topic_name;
   std::chrono::nanoseconds cycle_time;
 };
 
-class Sensor : public rclcpp::Node {
- public:
-  Sensor(const SensorSettings& settings) : Node(settings.node_name) {
+class Sensor : public rclcpp::Node
+{
+public:
+  Sensor(const SensorSettings & settings)
+  : Node(settings.node_name)
+  {
     publisher_ = this->create_publisher<message_t>(settings.topic_name, 10);
-    timer_ = this->create_wall_timer(settings.cycle_time,
-                                     [this] { timer_callback(); });
+    timer_ = this->create_wall_timer(
+      settings.cycle_time,
+      [this] {timer_callback();});
   }
 
- private:
-  void timer_callback() {
+private:
+  void timer_callback()
+  {
     auto message = publisher_->borrow_loaned_message();
     int64_t timestamp_in_ns =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(
-            std::chrono::system_clock::now().time_since_epoch())
-            .count();
+      std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::system_clock::now().time_since_epoch())
+      .count();
 
     const int64_t accumulated_latency_in_ns = 0;
 
@@ -36,7 +57,7 @@ class Sensor : public rclcpp::Node {
     publisher_->publish(std::move(message));
   }
 
- private:
+private:
   publisher_t publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
