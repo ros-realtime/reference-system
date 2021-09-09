c6e18e3658778b8df24ef879aeaec8c679414cfc
Christian Eltzschig <christian.eltzschig@apex.ai>
2021-09-09 19:37:16 +0200

    Sample tracing implemented

--- reference_system_autoware/include/reference_system_autoware/node/sensor.hpp
+++ reference_system_autoware/include/reference_system_autoware/node/sensor.hpp
@@ -18,46 +18,35 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "reference_system_autoware/sample_management.hpp"
 #include "reference_system_autoware/types.hpp"
 
-namespace node
-{
-struct SensorSettings
-{
+namespace node {
+struct SensorSettings {
   std::string node_name;
   std::string topic_name;
   std::chrono::nanoseconds cycle_time;
 };
 
-class Sensor : public rclcpp::Node
-{
-public:
-  Sensor(const SensorSettings & settings)
-  : Node(settings.node_name)
-  {
+class Sensor : public rclcpp::Node {
+ public:
+  Sensor(const SensorSettings& settings) : Node(settings.node_name) {
     publisher_ = this->create_publisher<message_t>(settings.topic_name, 10);
-    timer_ = this->create_wall_timer(
-      settings.cycle_time,
-      [this] {timer_callback();});
+    timer_ = this->create_wall_timer(settings.cycle_time,
+                                     [this] { timer_callback(); });
   }
 
-private:
-  void timer_callback()
-  {
+ private:
+  void timer_callback() {
     auto message = publisher_->borrow_loaned_message();
-    int64_t timestamp_in_ns =
-      std::chrono::duration_cast<std::chrono::nanoseconds>(
-      std::chrono::system_clock::now().time_since_epoch())
-      .count();
+    message.get().size = 0;
 
-    const int64_t accumulated_latency_in_ns = 0;
+    set_sample(this->get_name(), message.get());
 
-    message.get().data[0] = accumulated_latency_in_ns;
-    message.get().data[1] = timestamp_in_ns;
     publisher_->publish(std::move(message));
   }
 
-private:
+ private:
   publisher_t publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
 };
