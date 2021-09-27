390faeb308a5bdb8c8a68bb5a941c1e8f0c8cbb1
Evan Flynn <evan.flynn@apex.ai>
2021-09-27 10:06:33 -0700

    clean up memory_usage report generation
    Signed-off-by: Evan Flynn <evan.flynn@apex.ai>

--- autoware_reference_system/test/generate_reports.py
+++ autoware_reference_system/test/generate_reports.py
@@ -16,18 +16,26 @@ import sys
 
 from bokeh.io import export_png, output_file
 from bokeh.layouts import layout
-from bokeh.plotting import save
+from bokeh.plotting import save, show
 
-from callback_duration import individual, summary
+import callback_duration
+import memory_usage
 
 from tracetools_analysis.loading import load_file
+from tracetools_analysis.processor import Processor
+from tracetools_analysis.processor.memory_usage import KernelMemoryUsageHandler
+from tracetools_analysis.processor.memory_usage import UserspaceMemoryUsageHandler
 from tracetools_analysis.processor.ros2 import Ros2Handler
+from tracetools_analysis.utils.memory_usage import MemoryUsageDataModelUtil
 from tracetools_analysis.utils.ros2 import Ros2DataModelUtil
 
 path = ''
 pwd = ''
-symbols = None
-data_model = None
+ros2_data_model = None
+memory_data_model = None
+callback_symbols = None
+ust_memory_usage_dfs = None
+kernel_memory_usage_dfs = None
 
 SIZE_SUMMARY = 800
 SIZE_SUBPLOT = 500
@@ -50,43 +58,59 @@ def checkPath(p):
 
 def initTraceData():
     events = load_file(path)
-    handler = Ros2Handler.process(events)
-    handler.data.print_data()
+    ros2_handler = Ros2Handler.process(events)
+    ust_memory_handler = UserspaceMemoryUsageHandler()
+    kernel_memory_handler = KernelMemoryUsageHandler()
 
-    global data_model
-    data_model = Ros2DataModelUtil(handler.data)
+    global ros2_data_model
+    # callback duration
+    ros2_data_model = Ros2DataModelUtil(ros2_handler.data)
+    global callback_symbols
+    callback_symbols = ros2_data_model.get_callback_symbols()
 
-    global symbols
-    symbols = data_model.get_callback_symbols()
+    # memory usage
+    ros2_handler = Ros2Handler()
+    mem_proc = Processor(
+        ros2_handler,
+        ust_memory_handler,
+        kernel_memory_handler
+    )
+    mem_proc.process(events)
+
+    global memory_data_model
+    memory_data_model = MemoryUsageDataModelUtil(
+        userspace=ust_memory_handler.data,
+        kernel=kernel_memory_handler.data
+    )
 
 
 def memory_usage_report():
-    global path
-    global pwd
     output_file(
         filename=path + pwd + '_memory_usage_report.html',
         title='Memory Usage Report')
 
-    # duration_summary = summary(symbols, data_model=data_model, size=SIZE_SUMMARY)
-    # duration_individual = individual(symbols, data_model=data_model, size=SIZE_SUMMARY)
+    report = memory_usage.summary(
+        memory_data_util=memory_data_model,
+        ros2_data_util=ros2_data_model,
+        size=SIZE_SUMMARY)
 
-    # report = layout([[duration_summary], *duration_individual])
-
-    # show(report)
-    # export_png(report, filename = path + 'callback_duration_report.png')
+    show(report)
 
 
 def callback_report():
-    global path
-    global pwd
-    global data_model
 
     output_file(
         filename=path + pwd + '_callback_duration_report.html',
         title='Callback Duration Report')
 
-    duration_summary = summary(symbols, data_model=data_model, size=SIZE_SUMMARY)
-    duration_individual = individual(symbols, data_model=data_model, size=SIZE_SUBPLOT)
+    duration_summary = callback_duration.summary(
+        callback_symbols,
+        data_model=ros2_data_model,
+        size=SIZE_SUMMARY)
+    duration_individual = callback_duration.individual(
+        callback_symbols,
+        data_model=ros2_data_model,
+        size=SIZE_SUBPLOT)
 
     report = layout([[duration_summary], *duration_individual])
 
