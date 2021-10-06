b438055eba4cda9a57fee05df85c8f53825d98e1
Christophe Bedard <bedard.christophe@gmail.com>
2021-10-06 13:55:55 -0400

    Reduce list of enabled tracepoints down to minimum
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- autoware_reference_system/test/generate_callback_traces.py
+++ autoware_reference_system/test/generate_callback_traces.py
@@ -22,7 +22,6 @@ import launch_testing
 import launch_testing.actions
 
 from tracetools_launch.action import Trace
-from tracetools_trace.tools.names import DEFAULT_CONTEXT
 from tracetools_trace.tools.names import DEFAULT_EVENTS_ROS
 
 # Generate traces for specified executables and RMWs
@@ -57,20 +56,8 @@ def generate_test_description():
 
     trace_action = Trace(
         session_name='callback_' + test_exe_name + '_' + str(RUNTIME) + 's',
-        events_ust=[
-            'lttng_ust_cyg_profile_fast:func_entry',
-            'lttng_ust_cyg_profile_fast:func_exit',
-            'lttng_ust_statedump:start',
-            'lttng_ust_statedump:end',
-            'lttng_ust_statedump:bin_info',
-            'lttng_ust_statedump:build_id'
-        ] + DEFAULT_EVENTS_ROS,
-        events_kernel=[
-            'sched_switch'
-        ],
-        context_names=[
-            'ip',
-        ] + DEFAULT_CONTEXT,
+        events_ust=DEFAULT_EVENTS_ROS,
+        events_kernel=[],
     )
 
     launch_description.add_action(envvar_rcutils_action)
