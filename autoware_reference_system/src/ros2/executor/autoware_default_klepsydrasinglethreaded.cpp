80a8038fed27bc6586c039acb7cbdf5fdadb6a3c
paulagb8894 <paulagb8894@gmail.com>
2023-01-04 17:56:18 +0100

    removing environment

--- autoware_reference_system/src/ros2/executor/autoware_default_klepsydrasinglethreaded.cpp
+++ autoware_reference_system/src/ros2/executor/autoware_default_klepsydrasinglethreaded.cpp
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #include "rclcpp/rclcpp.hpp"
-#include <klepsydra/mem_core/mem_env.h>
 #include "reference_system/system/type/rclcpp_system.hpp"
 
 #include "autoware_reference_system/autoware_system_builder.hpp"
@@ -21,6 +20,7 @@
 #include "autoware_reference_system/system/timing/default.hpp"
 
 #include <kpsr_ros2_executor/streaming_executor_factory.hpp>
+#include "config.h"
 
 int main(int argc, char * argv[])
 {
@@ -33,52 +33,8 @@ int main(int argc, char * argv[])
 
   auto nodes = create_autoware_nodes<RclcppSystem, TimeConfig>();
 
-  kpsr::mem::MemEnv environment;
-  /*
-  environment.setPropertyString("container_name", "eventLoop_systemRef");
-  environment.setPropertyString("admin_log_filename", "");
-  environment.setPropertyString("stat_filename", "/home/ubuntu/development/ros2/ref_results/ros_ref.csv");
-  environment.setPropertyString("streaming_conf_file", "/home/ubuntu/development/ros2/ref_results/outputPolicy.json");
-  environment.setPropertyString("log_filename", "/home/ubuntu/development/ros2/ref_results/ros_ref.log");
-
-  environment.setPropertyInt("log_level", 2);
-  environment.setPropertyInt("pool_size", 100);
-  environment.setPropertyInt("number_of_cores", 3);
-  environment.setPropertyInt("critical_thread_pool_size", 16);
-  environment.setPropertyInt("non_critical_thread_pool_size", 1);
-  environment.setPropertyInt("number_of_parallel_threads", 0);
-  environment.setPropertyInt("stat_log_interval_ms", 1000);
-  environment.setPropertyInt("admin_log_interval", 1000);
-
-  environment.setPropertyBool("to_std_out", true);
-  environment.setPropertyBool("log_to_file", true);
-  environment.setPropertyBool("admin_log_to_file", false);
-  environment.setPropertyBool("stat_socket_container_enable", false);
-  environment.setPropertyBool("stat_file_container_enable", true);
-  environment.setPropertyBool("use_default_streaming_factory", true);
-  environment.setPropertyBool("test_dnn", false);
-  environment.setPropertyBool("export_streaming_configuration", true);
-  */
-  environment.setPropertyString("log_filename", "");
-  environment.setPropertyInt("log_level", 6);
-  environment.setPropertyInt("pool_size", 1);
-  environment.setPropertyInt("number_of_cores", 4);//1,2,3,4
-  environment.setPropertyInt("number_of_trypost", 0);
-  environment.setPropertyInt("critical_thread_pool_size", 32);
-  environment.setPropertyInt("non_critical_thread_pool_size", 32);
-  environment.setPropertyInt("number_of_parallel_threads", 0);
-
-  //
-  environment.setPropertyInt("stat_log_interval_ms", 1000);
-  environment.setPropertyBool("stat_file_container_enable", false);
-  environment.setPropertyString("stat_filename", "/home/ubuntu/development/klepsydra/reference_system/stats/benchmarks_internal_sub.csv");
-  //
-  environment.setPropertyString("streaming_conf_file", "/home/ubuntu/development/klepsydra/reference_system/policy.json");
-  environment.setPropertyBool("use_default_streaming_factory", false);
-  environment.setPropertyBool("test_dnn", false);
-  environment.setPropertyBool("export_streaming_configuration", false);
-
-  rclcpp::Executor::SharedPtr executor = kpsr::ros2::StreamingExecutorFactory::createExecutor(&environment);
+  const std::string jsonFileName = std::string(CONF_DATA) + std::string("/streaming_conf.json");
+  rclcpp::Executor::SharedPtr executor =  kpsr::ros2::StreamingExecutorFactory::createExecutor(jsonFileName);
 
   for (auto & node : nodes) {
     executor->add_node(node);
